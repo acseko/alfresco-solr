d1607125958cfd8ef3ec6f69da24795bee1800c5
András Csekő <andras.cseko@gmail.com>
2017-07-29 09:50:33 +0200

    update install path, adding core creation, fix for old lsof version

--- collectjars.sh
+++ collectjars.sh
@@ -1,8 +1,8 @@
-ls packaging/install/vanilla/exploded/alfresco-search-services/solr/server/lib/*.jar |cut -d/ -f 9 >exploded.jars
-ls packaging/install/vanilla/exploded/alfresco-search-services/solr/server/lib/ext/*.jar |cut -d/ -f 10 >>exploded.jars
-ls packaging/install/vanilla/exploded/alfresco-search-services/solr/server/solr-webapp/webapp/WEB-INF/lib/*.jar |cut -d/ -f 12 >>exploded.jars
+ls packaging/install/common/vanilla/exploded/alfresco-search-services/solr/server/lib/*.jar |cut -d/ -f 9 >exploded.jars
+ls packaging/install/common/vanilla/exploded/alfresco-search-services/solr/server/lib/ext/*.jar |cut -d/ -f 10 >>exploded.jars
+ls packaging/install/common/vanilla/exploded/alfresco-search-services/solr/server/solr-webapp/webapp/WEB-INF/lib/*.jar |cut -d/ -f 12 >>exploded.jars
 sort -u exploded.jars >exploded_sorted.jars
-ls packaging/install/lib/*.jar | grep -v src.jar |cut -d/ -f 4 | sort -u >lib_sorted.jars
+ls packaging/install/common/lib/*.jar | grep -v src.jar |cut -d/ -f 4 | sort -u >lib_sorted.jars
 rm exploded.jars
 
 
